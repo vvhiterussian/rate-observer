2eb95761db6ebd28e1d481e4c766e313c8cc2cbd
White Russian <whiterussian@Whites-iMac.Dlink>
2018-08-05 21:05:42 +0700

    init commit

---
+++ src/main/java/com/white/rateobserver/RateObserverApplication.java
@@ -0,0 +1,12 @@
+package com.white.rateobserver;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateObserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RateObserverApplication.class, args);
+    }
+}
